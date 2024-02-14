a6bf422d7e417f83e8065474d43220b9135b5bf4
javidannasib <javidan.code@gmail.com>
2024-02-14 15:19:14 +0400

    Initial Commit

---
+++ CardyUITests/CardyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CardyUITestsLaunchTests.swift
+//  CardyUITests
+//
+//  Created by Javidan Nasib on 14.02.24.
+//
+
+import XCTest
+
+final class CardyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
